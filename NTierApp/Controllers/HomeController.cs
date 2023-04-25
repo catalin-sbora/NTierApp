7848e12872e7b305ce57e954645f59d2bd6b7073
Catalin Sbora <catalin.sbora@gmail.com>
2023-04-25 11:42:01 +0300

    test for sonar cloud

--- NTierApp/Controllers/HomeController.cs
+++ NTierApp/Controllers/HomeController.cs
@@ -26,6 +26,7 @@ namespace NTierApp.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            int i = 0;
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
